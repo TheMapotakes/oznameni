38ad97531d3b690f4bb6ae663f9518fcac95f0b3
Sava Virtosu <savionok@gmail.com>
2014-07-18 13:39:01 +0300

    User address by IP

--- controllers/home.js
+++ controllers/home.js
@@ -2,9 +2,11 @@
  * GET /
  * Home page.
  */
+var geoip = require('geoip-lite');
 
 exports.index = function(req, res) {
   res.render('home', {
-    title: 'Home'
+    title: 'Home',
+    geo_location: geoip.lookup(req.connection.remoteAddress)
   });
 };
