7a0ddebecc99d96f68d1fc9bd5a0b50c02425339
Sava Virtosu <savionok@gmail.com>
2014-07-27 12:59:38 +0300

    changed title

--- controllers/user.js
+++ controllers/user.js
@@ -283,8 +283,8 @@ exports.postReset = function(req, res, next) {
       });
       var mailOptions = {
         to: user.email,
-        from: 'hackathon@starter.com',
-        subject: 'Your Hackathon Starter password has been changed',
+        from: 'sistari.md@gmail.com',
+        subject: 'Your Sistari.md password has been changed',
         text: 'Hello,\n\n' +
           'This is a confirmation that the password for your account ' + user.email + ' has just been changed.\n'
       };
@@ -361,8 +361,8 @@ exports.postForgot = function(req, res, next) {
       });
       var mailOptions = {
         to: user.email,
-        from: 'hackathon@starter.com',
-        subject: 'Reset your password on Hackathon Starter',
+        from: 'sistari.md@gmail.com',
+        subject: 'Reset your password on Sistari.md',
         text: 'You are receiving this email because you (or someone else) have requested the reset of the password for your account.\n\n' +
           'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
           'http://' + req.headers.host + '/reset/' + token + '\n\n' +
